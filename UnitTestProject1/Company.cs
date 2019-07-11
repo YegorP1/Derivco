5c17c98900f617ebcbf2ac73c20bd08e035aad42
Yegor <Yegor@DESKTOP-SP0RH04>
2019-07-12 01:39:07 +0300

    Create BaseRestAPi class with main methods Create TestCases class with test flow Create class Company and Employee

--- UnitTestProject1/Company.cs
+++ UnitTestProject1/Company.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace UnitTestProject1
 {
     class Company
     {
-
+        public RestClient _client;
+        public RestRequest _request;
+        public IRestResponse _response;
+        public JToken _access;
+        public string _id;
     }
 }

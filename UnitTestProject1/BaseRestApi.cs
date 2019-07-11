ffbb4810cb04948ed56b9e2102c0df4dcddde929
Yegor <Yegor@DESKTOP-SP0RH04>
2019-07-12 01:28:21 +0300

    Add project files.

---
+++ UnitTestProject1/BaseRestApi.cs
@@ -0,0 +1,105 @@
+﻿
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+
+namespace UnitTestProject1
+{
+ 
+        class BaseRestApi
+    {
+        public RestClient _client;
+        public RestRequest _request;
+        public IRestResponse _response;
+        public JToken _access;
+        public string _id;
+
+        public string _baseUrl = "https://mobilewebserver9-pokertest8ext.installprogram.eu/TestApi";
+        public string _tokenBody = "grant_type=password&username=testName&password=test";
+        public int _random = GetRandomNumber();
+
+        public static int GetRandomNumber()
+        {
+            var random = new Random();
+            return random.Next(); 
+        }
+
+
+        //Generate OAuth2 Token
+        public IRestResponse GenerateToken()
+        {
+            _client = new RestClient(_baseUrl);      
+            _request = new RestRequest("/token", Method.POST);
+            _request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            string encodedBody = string.Format(_tokenBody);
+            _request.AddParameter("application/x-www-form-urlencoded", encodedBody, ParameterType.RequestBody);
+
+            return _client.Execute(_request);
+        }
+
+        //Retrieve bearer token 
+        public string RetrieveToken(IRestResponse response)
+        {
+            var content = response.Content;
+            JObject obj = JObject.Parse(content);
+            _access= obj.SelectToken("access_token");
+            return _access.ToString();         
+        }
+
+        //Create POST request
+        public IRestResponse PostRequest(string bearerToken)
+        {
+            _client = new RestClient(_baseUrl);
+            _request = new RestRequest("/api/automation/companies", Method.POST);
+            _request.AddHeader("Content-Type", "application/json");
+            _request.AddHeader("Authorization", "Bearer " + bearerToken);
+             var body = new { Name = "TestCompany" + _random };
+            _request.AddJsonBody(body);
+
+            return _client.Execute(_request);
+        }
+
+        //Create GET request
+        public IRestResponse GetRequest(string bearerToken)
+        {
+            _client = new RestClient(_baseUrl);
+            _request = new RestRequest("/api/automation/companies", Method.GET);
+            _request.AddHeader("Content-Type", "application/json");
+            _request.AddHeader("Authorization", "Bearer " + bearerToken);
+
+            return _client.Execute(_request);
+        }
+
+        //Create GET BY ID request
+        public IRestResponse GetByIdRequest(string bearerToken, string inputId)
+        {
+            _client = new RestClient(_baseUrl);
+            _request = new RestRequest("/api/automation/companies/id/" + inputId, Method.GET);
+            _request.AddHeader("Authorization", "Bearer " + bearerToken);
+
+            return _client.Execute(_request);
+        }
+
+        //Retrieve ID  
+        public string RetrieveId(string inputJson)
+        {
+            string key = inputJson.Substring(inputJson.IndexOf("Id"));
+            key = key.Substring(key.IndexOf("\""), key.IndexOf(",") - key.IndexOf("\""));
+            key = key.Trim('\"').Substring(1);
+
+            return key;
+        }
+
+        //Create DELETE request
+        public IRestResponse DeleteRequest(string bearerToken, string inputId)
+        {
+            _client = new RestClient(_baseUrl);
+            _request = new RestRequest("/api/automation/companies/id/" + inputId, Method.DELETE);
+            _request.AddHeader("Authorization", "Bearer " + bearerToken);
+
+            return _client.Execute(_request);
+        }
+
+
+    }
+}
---
+++ UnitTestProject1/TestCases.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+ 
+    public class TestCases
+    {
+        BaseRestApi restApi = new BaseRestApi();
+       
+        public string createToken()
+        {
+            var tokenResponse = restApi.GenerateToken();
+            var tokenContent = tokenResponse.Content;
+            var bearer = restApi.RetrieveToken(tokenResponse);
+            Console.WriteLine(bearer);
+            return bearer;
+        }
+
+        public string getCompanyId()
+        {
+            var response = restApi.GetRequest(createToken());
+            var content = response.Content;
+            string companyID = restApi.RetrieveId(content);
+            return companyID;
+        }
+
+
+        [TestMethod, Order(1)]
+        public void CreateCompany()
+        {
+            var postCompanyResponse = restApi.PostRequest(createToken());
+            var postCompanyContent = postCompanyResponse.Content;
+            Console.WriteLine(postCompanyResponse.StatusCode.ToString());
+
+        }
+
+        [TestMethod, Order(2)]
+
+        public void GetAllCompanies()
+        {
+            var allCompaniesResponse = restApi.GetRequest(createToken());
+            var allCompaniesContent = allCompaniesResponse.Content;
+            var compId = restApi.RetrieveId(allCompaniesContent);
+
+            Console.WriteLine(allCompaniesResponse.StatusCode.ToString());
+            Console.WriteLine(allCompaniesContent);
+            
+        }
+
+        [TestMethod, Order(3)]
+
+        public void GetCompanyById()
+        {
+            var getCompanyResponse = restApi.GetByIdRequest(createToken(), getCompanyId());
+            var getCompanyContent = getCompanyResponse.Content;
+            
+            Console.WriteLine(getCompanyResponse.StatusCode.ToString());
+            Console.WriteLine(getCompanyContent);
+
+        }
+
+        [TestMethod, Order(4)]
+        public void DeleteCompany()
+        {
+            var deleteCompanyResponse = restApi.DeleteRequest(createToken(), getCompanyId());
+            var deleteCompanyContent = deleteCompanyResponse.Content;
+            Console.WriteLine(deleteCompanyResponse.StatusCode.ToString());
+
+        }
+
+    }
+}
